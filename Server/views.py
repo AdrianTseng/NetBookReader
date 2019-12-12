ea6b51d15a5a999e63f499c70d75a8ad2714dada
AdrianZheng <mail@qmz.me>
2019-12-12 09:56:10 +0800

    优化了检索流程

--- Server/views.py
+++ Server/views.py
@@ -181,16 +181,16 @@ def search_for_add_inventory():
 
     book_name = request.form.get("book")
 
-    prefix_url = "https://m.qu.la"
+    prefix_url = "https://m.jx.la"
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'en',
-        'Accept-Encoding':'gzip, deflate, sdch',
-        'Accept-Language':'zh-CN,zh;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-        'Cache-Control':'max-age=0',
-        'Connection':'keep-alive',
-        'Referer': "https://m.qu.la"
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Referer': prefix_url
     }
 
     prefix_html = requests.get(prefix_url, headers)
